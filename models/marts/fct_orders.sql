722cba8d996f58eb8c68b7f3c50f369c38788919
Robert Landler <gwendoline.schissendorf@gmx.at>
2026-01-16 12:35:03 +0000

    staging files and marts files created

---
+++ models/marts/fct_orders.sql
@@ -0,0 +1,19 @@
+with orders as  (
+   
+   select order_id,
+        customer_id,
+        order_date,
+        status
+   from {{ ref('stg_orders') }}
+),
+
+final as (
+
+   select
+       orders.*
+   from orders
+   
+)
+
+select * 
+from final
